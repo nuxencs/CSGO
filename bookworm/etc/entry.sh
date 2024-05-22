e674d525412e1349e1f2330c585be0deceaf835b
nuxen <theonenuxen@proton.me>
2024-05-22 22:44:23 +0200

    feat: skin plugins, NoLobbyReservation & automated docker builds (#1)
    * feat: skin plugins, NoLobbyReservation & automated docker builds
    
    * fix: dockerfile parse error
    
    * fix: dockerfile
    
    * chore: use bookworm and fix docker build
    
    * fix: update docker context
    
    * fix: use buildx again
    
    * fix: docker build output
    
    * chore: build different tags

--- bookworm/etc/entry.sh
+++ bookworm/etc/entry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p "${STEAMAPPDIR}" || true  
+mkdir -p "${STEAMAPPDIR}" || true
 
 bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
 				+login anonymous \
@@ -9,13 +9,54 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
 # Are we in a metamod container and is the metamod folder missing?
 if  [ ! -z "$METAMOD_VERSION" ] && [ ! -d "${STEAMAPPDIR}/${STEAMAPP}/addons/metamod" ]; then
 	LATESTMM=$(wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/mmsource-latest-linux)
-	wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/"${LATESTMM}" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"	
+	wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/"${LATESTMM}" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"
 fi
 
 # Are we in a sourcemod container and is the sourcemod folder missing?
 if  [ ! -z "$SOURCEMOD_VERSION" ] && [ ! -d "${STEAMAPPDIR}/${STEAMAPP}/addons/sourcemod" ]; then
 	LATESTSM=$(wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/sourcemod-latest-linux)
 	wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/"${LATESTSM}" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"
+fi
+
+# Install NoLobbyReservation
+if  [ ! -z "$SOURCEMOD_VERSION" ] && [ ! -f "${STEAMAPPDIR}/${STEAMAPP}/addons/sourcemod/plugins/nolobbyreservation.smx"]; then
+	wget -qO nlr.zip https://github.com/nuxencs/NoLobbyReservation/releases/latest/download/NoLobbyReservation.zip && \
+	unzip -d "${STEAMAPPDIR}/${STEAMAPP}" nlr.zip && \
+	rm nlr.zip
+fi
+
+# Install PTaH
+if  [ ! -z "$SOURCEMOD_VERSION" ] && [ "$SKIN_PLUGINS" == "true" ] && [ ! -f "${STEAMAPPDIR}/${STEAMAPP}/addons/sourcemod/extensions/PTaH.ext.2.csgo.so"]; then
+	wget -qO ptah.zip $(curl -s https://api.github.com/repos/komashchenko/PTaH/releases/latest | grep download | grep linux | cut -d\" -f4) && \
+	unzip -d "${STEAMAPPDIR}/${STEAMAPP}" ptah.zip && \
+	rm ptah.zip
+fi
+
+# Install Weapons
+if  [ ! -z "$SOURCEMOD_VERSION" ] && [ "$SKIN_PLUGINS" == "true" ] && [ ! -f "${STEAMAPPDIR}/${STEAMAPP}/addons/sourcemod/plugins/weapons.smx"]; then
+	wget -qO wpns.zip $(curl -s https://api.github.com/repos/kgns/weapons/releases/latest | grep download | grep weapons | cut -d\" -f4) && \
+	unzip -d "${STEAMAPPDIR}/${STEAMAPP}" wpns.zip && \
+	rm wpns.zip
+
+	# Remove prefix & set float increment size
+	sed -i -e 's:sm_weapons_chat_prefix "[oyunhost.net]":sm_weapons_chat_prefix "":g' "${STEAMAPPDIR}/${STEAMAPP}/cfg/sourcemod/weapons.cfg"
+	sed -i -e 's:sm_weapons_float_increment_size "0.05":sm_weapons_float_increment_size "0.01":g' "${STEAMAPPDIR}/${STEAMAPP}/cfg/sourcemod/weapons.cfg"
+fi
+
+# Install Gloves
+if  [ ! -z "$SOURCEMOD_VERSION" ] && [ "$SKIN_PLUGINS" == "true" ] && [ ! -f "${STEAMAPPDIR}/${STEAMAPP}/addons/sourcemod/plugins/gloves.smx"]; then
+	wget -qO glvs.zip $(curl -s https://api.github.com/repos/kgns/gloves/releases/latest | grep download | grep gloves | cut -d\" -f4) && \
+	unzip -d "${STEAMAPPDIR}/${STEAMAPP}" glvs.zip && \
+	rm glvs.zip
+
+	# Remove prefix & set float increment size
+	sed -i -e 's:sm_gloves_chat_prefix "[oyunhost.net]":sm_gloves_chat_prefix "":g' "${STEAMAPPDIR}/${STEAMAPP}/cfg/sourcemod/gloves.cfg"
+	sed -i -e 's:sm_gloves_float_increment_size "0.2":sm_gloves_float_increment_size "0.01":g' "${STEAMAPPDIR}/${STEAMAPP}/cfg/sourcemod/gloves.cfg"
+fi
+
+# Changing FollowCSGOServerGuidelines to "no" for skin plugins
+if  [ ! -z "$SOURCEMOD_VERSION" ] && [ "$SKIN_PLUGINS" == "true" ]; then
+	sed -i -e 's:"FollowCSGOServerGuidelines"\t"yes":"FollowCSGOServerGuideLines"\t"no":g' "${STEAMAPPDIR}/${STEAMAPP}/addons/sourcemod/configs/core.cfg"
 fi
 
 # Is the config missing?
@@ -51,7 +92,7 @@ sv_password	+sv_password "${SRCDS_PW}"
 rcon_password	+rcon_password "${SRCDS_RCONPW}"
 EOM
 	# if autoexec is present, drop overwritten arguments here (example: SRCDS_PW & SRCDS_RCONPW)
-	bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console -autoupdate \
+	bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console \
 				-steam_dir "${STEAMCMDDIR}" \
 				-steamcmd_script "${HOMEDIR}/${STEAMAPP}_update.txt" \
 				-usercon \
@@ -77,7 +118,7 @@ EOM
 				"${ADDITIONAL_ARGS}"
 else
 	# If no autoexec is present, use all parameters
-	bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console -autoupdate \
+	bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console \
 				-steam_dir "${STEAMCMDDIR}" \
 				-steamcmd_script "${HOMEDIR}/${STEAMAPP}_update.txt" \
 				-usercon \
